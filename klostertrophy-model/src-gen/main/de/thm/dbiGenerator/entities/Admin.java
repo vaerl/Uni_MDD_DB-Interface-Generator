e60adda926600ab366e7cf15d753e27a3d569aa0
Lukas Müller <lm071120@gmail.com>
2020-07-31 13:03:35 +0200

    Remove unneccessary files.

--- klostertrophy-model/src-gen/main/de/thm/dbiGenerator/entities/Admin.java
+++
@@ -1,9 +0,0 @@
-package de.thm.dbiGenerator.entities;
-
-import de.thm.dbiGenerator.entities.AdminGen;
-import javax.persistence.*;
-
-@Entity
-public class Admin extends AdminGen {
-	
-}
--- klostertrophy-model/src-gen/main/de/thm/dbiGenerator/entities/Game.java
+++
@@ -1,9 +0,0 @@
-package de.thm.dbiGenerator.entities;
-
-import de.thm.dbiGenerator.entities.GameGen;
-import javax.persistence.*;
-
-@Entity
-public class Game extends GameGen {
-	
-}
